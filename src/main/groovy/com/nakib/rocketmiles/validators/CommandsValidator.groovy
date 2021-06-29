eaf7b991d845c8ba96a0c84b5fc724d6429a5130
Nakib A. Khan <nakib.khan1988@gmail.com>
2021-06-29 07:45:31 -0500

    (add) add validation

--- src/main/groovy/com/nakib/rocketmiles/validators/CommandsValidator.groovy
+++ src/main/groovy/com/nakib/rocketmiles/validators/CommandsValidator.groovy
@@ -1,8 +1,31 @@
 package com.nakib.rocketmiles.validators
 
+import com.nakib.rocketmiles.constants.Commands
+
 class CommandsValidator {
 
     static boolean isValid(String input)   {
-        true
+        String [] inputs = input.split()
+        if(inputs[0] == Commands.EXIT)  {
+            return true
+        }
+
+        if(inputs[0] == Commands.SHOW)  {
+            return true
+        }
+
+        if(inputs[0] == Commands.TAKE || inputs[0] == Commands.PUT) {
+            if(inputs.size() == 6)  {
+                for(int i = 1; i< inputs.size(); i++)   {
+                    if(! inputs[i].isNumber())  {
+                        return false
+                    }
+                }
+            }
+
+            return true
+        }
+
+        false
     }
 }
---
+++ src/test/groovy/com/nakib/rocketmiles/validators/CommandsValidatorTest.java
@@ -0,0 +1,15 @@
+package com.nakib.rocketmiles.validators;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//class CommandsValidatorTest {
+//    CommandsValidator
+//
+//    @Test
+//    void isValid_exit() {
+//        Assertions.assertTrue();
+//    }
+//}
